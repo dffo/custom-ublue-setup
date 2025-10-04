3b3e32cd6e52981b44de6d3f08942d8d7253bc94
Derek Furtado <furtadod@mcmaster.ca>
2025-10-03 22:26:52 -0400

    update

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,6 +16,7 @@ dnf5 install -y featherpad
 dnf5 install -y rclone
 dnf5 install -y dconf-editor
 dnf5 install -y racket
+dnf5 install -y xwayland-satellite
 #raco pkg install pollen #nah, doesn't work---tries to make .local in root
 
 # Use a COPR Example:
