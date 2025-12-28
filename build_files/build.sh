73b7db732798761c1532a01fc608bee4ca903640
Derek Furtado <furtadod@mcmaster.ca>
2025-12-28 18:27:49 -0500

    update

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 dnf5 install -y vim
+dnf5 install -y emacs
 dnf5 install -y fastfetch
 dnf5 install -y featherpad
 dnf5 install -y rclone
